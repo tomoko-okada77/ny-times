4a3526acb4078935a07462ff8f0b619b5750fab1
Tomoko Okada <tomoko.okada77@gmail.com>
2025-06-18 14:58:48 +0900

    [feat] 記事一覧の取得

---
+++ src/services/nytimes.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { ArticleSearchParams, ArticleSearchResponse } from "@/types/article";
+
+export const nytimesApi = createApi({
+  reducerPath: "nytimesApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.nytimes.com/svc/search/v2",
+  }),
+  endpoints: (builder) => ({
+    searchArticles: builder.query<ArticleSearchResponse, ArticleSearchParams>({
+      query: ({ q = "", fq = "", page = 0 }) => ({
+        url: "articlesearch.json",
+        params: {
+          q,
+          fq,
+          page,
+          "api-key": process.env.NEXT_PUBLIC_NYT_API_KEY,
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useSearchArticlesQuery } = nytimesApi;
---
+++ src/types/article.d.ts
@@ -0,0 +1,61 @@
+export type ArticleSearchParams = {
+  q?: string;
+  fq?: string;
+  page?: number;
+};
+
+export type ArticleSearchResponse = {
+  status: string;
+  copyright: string;
+  response: {
+    docs: Array<{
+      abstract: string;
+      byline: {
+        original: string;
+      };
+      document_type: string;
+      headline: {
+        main: string;
+        kicker: string;
+        print_headline: string;
+      };
+      _id: string;
+      keywords: Array<{
+        name: string;
+        value: string;
+        rank: number;
+      }>;
+      multimedia: {
+        caption: string;
+        credit: string;
+        default: {
+          url: string;
+          height: number;
+          width: number;
+        };
+        thumbnail: {
+          url: string;
+          height: number;
+          width: number;
+        };
+      };
+      news_desk: string;
+      print_page: string;
+      print_section: string;
+      pub_date: string;
+      section_name: string;
+      snippet: string;
+      source: string;
+      subsection_name: string;
+      type_of_material: string;
+      uri: string;
+      web_url: string;
+      word_count: number;
+    }>;
+    meta: {
+      hits: number;
+      offset: number;
+      time: number;
+    };
+  };
+};
